53a2679facf16f5d14949a6746f9a50c3a89ed8e
NovoAcesso <novo@acesso.com>
2018-05-25 15:56:16 -0400

    Projeto mult

--- config/conexao.js
+++ config/conexao.js
@@ -1,8 +1,8 @@
 module.exports = {
     'auth': {
-        'host': '54.233.66.37',
-        'user': 'dys',
-        'password': 'Xbac0n@dys',
+        'host': '127.0.0.1',
+        'user': 'root',
+        'password': 'smB@Joh6',
         'database': 'node'
     }
 }
